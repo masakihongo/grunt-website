08d1770de19f209a14d28c5c78f63c89a16a82cf
masakihongo <hongo@egaoinc.jp>
2014-02-05 11:38:12 +0900

    init functions setup

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,7 +1,39 @@
 (function ($, window) {
     'use strict';
-    window.app = {};
+    var app = window.appName = {};
+
+    /* -----------------------------------------------------------------
+     * Initialize
+    ----------------------------------------------------------------- */
+    app.init = function () {
+        var cmn = {};
+
+        cmn.init = function () {
+            app.functions.addLog('init!');
+        };
+
+        return cmn;
+    };
+
+    /* -----------------------------------------------------------------
+     * Utility functions
+    ----------------------------------------------------------------- */
+    app.functions = (function () {
+        var module = {
+
+            addLog: function (log) {
+                console.log(log);
+            }
+        };
+
+        return module;
+    }());
+
+    /* -----------------------------------------------------------------
+     * Setup
+    ----------------------------------------------------------------- */
     $(function () {
-        console.log('Hello. World!');
+        var app = window.appName.init();
+        app.init();
     });
 }(jQuery, window));
