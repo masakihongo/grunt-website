1d08006a02eb1b9016969b35904e0915168ca754
masakihongo <hongo@egaoinc.jp>
2014-02-05 11:35:41 +0900

    load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,15 +64,7 @@ module.exports = function (grunt) {
     }
   });
 
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('default', ['connect', 'watch']);
   grunt.registerTask('dev', ['connect', 'watch']);
