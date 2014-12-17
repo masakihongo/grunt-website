485513a34cc07496a197aff5f5ae873824e0e2ac
masakihongo <hongo@egaoinc.jp>
2014-12-17 17:07:03 +0900

    [update] grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,145 @@
+'use strict';
+
 module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    dir: {
-        // Configurable paths
-        dev: 'dev/',
-        dist: 'dist/'
-    },
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        dir: {
+            // Configurable paths
+            dev: 'dev/',
+            dist: 'dist/',
+            asset: 'share/'
+        },
 
-    connect: {
-      options: {
-      },
-      livereload: {
-        options: {
-          base: '<%= dir.dev %>',
-          open: true
-        }
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      dist: {
-        files: {
-          '<%= dir.dist %>js/script.js': ['<%= dir.dev %>js/script.js']
-        }
-      }
-    },
-    compass: {
-      dist: {
-        options: {
-          sassDir: '<%= dir.dev %>_sass',
-          cssDir: '<%= dir.dev %>css',
-          imagesDir: '<%= dir.dev %>_sass/sprite',
-          generatedImagesDir: '<%= dir.dev %>img',
-          httpGeneratedImagesPath: '/img',
-          javascriptsDir: '<%= dir.dev %>js',
-          fontsDir: '<%= dir.dev %>fonts',
-          outputStyle: 'compact',
-          noLineComments: true,
-          relativeAssets: false,
-          assetCacheBuster: false
-        }
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: ['<%= dir.dev %>js/*.js']
-    },
-    copy: {
-      dist: {
-        expand: true,
-        cwd: '<%= dir.dev %>',
-        dest: '<%= dir.dist %>',
-        src: '**'
-      }
-    },
-    clean: {
-      build: ['<%= dir.dist %>_sass']
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      js: {
-        files: ['<%= dir.dev %>js/*.js'],
-        tasks: ['jshint']
-      },
-      compass: {
-        files: ['<%= dir.dev %>_sass/{,*/}*.scss'],
-        tasks: ['compass']
-      },
-      livereload: {
-        files: ['<%= dir.dev %>{,*/}*.html']
-      }
-    },
-    concat: {
-      files: {
-        src: '<%= dir.dev %>lib/plugins/**.js',
-        dest: '<%= dir.dev %>lib/js/plugins.js'
-      }
-    }
-  });
+        watch: {
+            js: {
+                files: ['<%= dir.dev %><%= dir.asset %>js/*.js'],
+                tasks: ['jshint']
+            },
+            concat: {
+                files: ['<%= dir.dev %><%= dir.asset %>js/plugins/*.js'],
+                tasks: ['concat']
+            },
+            compass: {
+                files: ['<%= dir.dev %><%= dir.asset %>sass/{,*/}*.scss'],
+                tasks: ['compass']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= dir.dev %>**/*.html',
+                    '<%= dir.dev %>**/*.js',
+                    '<%= dir.dev %>**/*.css'
+                ]
+            }
+        },
 
-  require('load-grunt-tasks')(grunt);
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    base: '<%= dir.dev %>',
+                    open: true
+                }
+            }
+        },
 
-  grunt.registerTask('default', ['concat', 'connect', 'watch']);
-  grunt.registerTask('build', ['concat', 'compass', 'copy', 'clean', 'uglify']);
+        compass: {
+            dist: {
+                options: {
+                    sassDir: '<%= dir.dev %><%= dir.asset %>sass',
+                    cssDir: '<%= dir.dev %><%= dir.asset %>css',
+                    imagesDir: '<%= dir.dev %><%= dir.asset %>img/sprite',
+                    generatedImagesDir: '<%= dir.dev %><%= dir.asset %>img',
+                    httpGeneratedImagesPath: '/<%= dir.asset %>/img',
+                    javascriptsDir: '<%= dir.dev %><%= dir.asset %>js',
+                    fontsDir: '<%= dir.dev %><%= dir.asset %>fonts',
+                    outputStyle: 'compact',
+                    noLineComments: true,
+                    relativeAssets: false,
+                    assetCacheBuster: false
+                }
+            }
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= dir.dev %><%= dir.asset %>js/*.js'
+            ]
+        },
+
+        uglify: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dir.dev %><%= dir.asset %>js',
+                    src: '*.js',
+                    dest: '<%= dir.dist %><%= dir.asset %>js'
+                }]
+            }
+        },
+
+        cssmin: {
+            files: {
+                expand: true,
+                cwd: '<%= dir.dev %><%= dir.asset %>css/',
+                dest: '<%= dir.dist %><%= dir.asset %>css/',
+                src: ['*.css']
+            }
+        },
+
+        concat: {
+            files: {
+                src: '<%= dir.dev %><%= dir.asset %>js/plugins/**.js',
+                dest: '<%= dir.dev %><%= dir.asset %>js/lib/plugins.js'
+            }
+        },
+
+        copy: {
+            dist: {
+                expand: true,
+                cwd: '<%= dir.dev %>',
+                dest: '<%= dir.dist %>',
+                src: '**'
+            }
+        },
+
+        clean: {
+            all: {
+                src: ['<%= dir.dist %>']
+            },
+            dist: {
+                src: ['<%= dir.dist %><%= dir.asset %>js/plugins', '<%= dir.dist %><%= dir.asset %>_sass']
+            }
+
+        }
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', [
+        'concat',
+        'compass',
+        'connect:livereload',
+        'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:all',
+        'concat',
+        'compass',
+        'copy',
+        'clean:dist',
+        'uglify',
+        'cssmin'
+    ]);
 };
